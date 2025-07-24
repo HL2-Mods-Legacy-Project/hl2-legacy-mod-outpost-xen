00e6cc9ec5dca509887c9f1b133567096a9f3e25
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-24 16:29:53 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,9 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
-  -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -Version "1.0.0" `
+  -OutputFilePrefix "Outpost_Xen" `
+  -ModName "Outpost Xen" `
+  -ModFolder "outpost_xen" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/outpost-xen/Outpost-Xen-Original-Fixed.zip" `
+  -ModBaseFilesUrlHash "4e81cbd6eba498f3bf171211a4cb1adddce61f4961ba39a749e2d32f51efb4c8" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
